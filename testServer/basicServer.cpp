166036b1153d7b5506829bd2e84856fde623f0cf
volpe_p <philippe.volpe@epitech.eu>
2014-11-12 22:26:16 +0100

    basic server

--- testServer/basicServer.cpp
+++ testServer/basicServer.cpp
@@ -23,23 +23,22 @@ int				runBasicServer()
 	ITCPServer	*server = new myTCPServer();
 
 	port = DEFAULT_SERVER_PORT;
-	server->start(ip, port, 10); //On démare le server
+	server->start(ip, port, 10); //On démmare le server
 
 	std::cout << "Server started" << std::endl;
 
 	ITCPRemoteClient *client = server->acceptClient(); // On accepte la connexion du client
 	std::cout << "Client connected" << std::endl;
 	
-	
 	PacketHelper::writeTcpHeader(packet, RequestAuth);
-	PacketHelper::writeTcpHeaderSize(packet);	//Creer un packet vide (avec command id = RequestAuth)
+	PacketHelper::writeTcpHeaderSize(packet);	//Création d'un packet vide (avec command id = RequestAuth)
 	client->prepareMsg(packet, packet.length());
 	client->send(); //On send
 	std::cout << "Auth msg sended" << std::endl;
 
 	{
 		packet = "";
-		size = (*client >> packet);  //On reçois
+		size = (*client >> packet);  //On reçoit
 		std::istringstream stream(packet);
 		PacketHelper::readTcpHeader(stream, header); // On read le header reçu
 		std::cout << "Receive packet with id " << header.commandId << std::endl;
@@ -49,8 +48,16 @@ int				runBasicServer()
 		std::string usr = Serializer::deserialize<std::string>(stream); // username
 		std::string passwd = Serializer::deserialize<std::string>(stream); //password
 		std::cout << "Identification username = " << usr << " password : " << passwd << std::endl;
-		system("PAUSE");
-
 		//En answer
 	}
+
+	packet = "";
+	PacketHelper::writeTcpHeader(packet, SendFriendsList); // Création d'un packet vide (avec commande id = SendFriendList)
+	std::list<std::pair<std::string, char>> listFriend;
+	packet = packet.append(Serializer::serialize<std::list<std::pair<std::string, char>>>(listFriend));
+	PacketHelper::writeTcpHeaderSize(packet);
+	client->prepareMsg(packet, packet.length()); 
+	client->send(); // On envoi
+	std::cout << "SendFriendList request sended" << std::endl;
+	system("PAUSE");
 }
